981bb64c968c85aa07c92a1dab17a4ecc049c15d
sanghunlee <simoniful0gmail.com>
2021-06-23 18:40:25 +0900

    Add: nav layout complete

--- src/Routes.js
+++ src/Routes.js
@@ -1,10 +1,17 @@
 import React from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import Login from './Pages/Login';
+import Main from './Pages/Main';
+import GradeSelect from './Pages/GradeSelect';
+import OrderForm from './Pages/OrderForm';
+import Confirm from './Pages/Confirm';
+import Nav from './Components/Nav';
 
 class Routes extends React.Component {
   render() {
     return (
       <Router>
+        <Nav />
         <Switch>
           <Route exact path="/login" component={Login} />
           <Route exact path="/main" component={Main} />
